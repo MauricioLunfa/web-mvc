049881b60f4235e2e57f72661b4e74b3bbf0b7a8
Mauricio Barreto <mauriciobarretom@hotmail.com>
2019-03-28 15:56:13 -0300

    Terceira aulda do curso web mvc

---
+++ webMVC/Data/webMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace webMVC.Models
+{
+    public class webMVCContext : DbContext
+    {
+        public webMVCContext (DbContextOptions<webMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<webMVC.Models.Departamento> Departamento { get; set; }
+    }
+}
